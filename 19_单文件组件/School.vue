f8c2e6e29f9bfd6de51260e690ee1eb5772a6c2c
liqiuliang <liqiuliang@bilibili.com>
2023-11-21 00:42:30 +0800

    format

--- 19_单文件组件/School.vue
+++ 19_单文件组件/School.vue
@@ -1,30 +1,33 @@
 <template>
-	<div class="demo">
-		<h2>学校名称：{{name}}</h2>
-		<h2>学校地址：{{address}}</h2>
-		<button @click="showName">点我提示学校名</button>	
-	</div>
+<!--    组件的结构html-->
+    <div class="demo">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
 </template>
 
 <script>
-	 export default {
-		name:'School',
-		data(){
-			return {
-				name:'尚硅谷',
-				address:'北京昌平'
-			}
-		},
-		methods: {
-			showName(){
-				alert(this.name)
-			}
-		},
-	}
+//    组件交互的代码（数据、方法等等）js
+    export default {
+        name: 'School',
+        data() {
+            return {
+                name: '尚硅谷',
+                address: '北京昌平'
+            }
+        },
+        methods: {
+            showName() {
+                alert(this.name)
+            }
+        },
+    }
 </script>
 
 <style>
-	.demo{
-		background-color: orange;
-	}
+/*    组件的样式css*/
+    .demo {
+        background-color: #cb15a2;
+    }
 </style>
